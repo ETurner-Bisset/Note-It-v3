24ee5387d71870aa740f013da9cbc27309361a66
ETurner-Bisset <eturnerbisset@gmail.com>
2024-03-15 11:14:01 +0000

    Troubleshoot proxy settings

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.get('/ip', (request, response) => response.send(request.ip));
+
 app.use(cors());
 
 // Helmet
